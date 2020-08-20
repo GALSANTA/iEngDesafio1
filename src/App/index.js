f4f9ac9d9e1515fd06004ba70b0493807233c799
Fernando Marques <fernando.silva@ccc.ufcg.edu.br>
2020-08-20 18:51:48 -0300

    Criando página da cidade

---
+++ src/App/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './style.css';
+
+function City() {
+
+    const [data, setDia] = useState();
+
+    useEffect(() => {
+        navigator.geolocation.watchPosition(api);
+    }, []);
+
+    async function api({ coords }) {
+
+        // 35.6684415 139.6007804 tokyo, Nippon
+        // 21.3282956 -157.9391619 Havaí, EUA
+        // -15.7745457 -48.3575684 Brasília, Brasil
+        // -3.044653 -60.1071907 Manaus, Brasil
+
+        const { latitude, longitude } = coords;
+
+        var d = await axios.get(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`);
+        var sunrise = d.data.results.sunrise.split(" ")[0];
+        var sunset = d.data.results.sunset.split(" ")[0];
+
+        var data = new Date();
+ 
+        var hora = data.getHours();
+        
+        if (hora > 11 && hora <= 23) {
+            if (hora >= parseInt(sunrise[0]) && hora< parseInt(sunset[0]) + 12) {
+                setDia('dia');
+            } else {
+                setDia('noite');
+            }
+        } else {
+            if (hora >= parseInt(sunrise[0]) && hora < parseInt(sunset[0]) + 12) {
+                setDia('dia');
+            } else {
+                setDia('noite');
+            }
+        }
+    }
+
+    return (
+        <div className={`row cidade ${data}`}>
+            <h1>PREDIOS</h1>
+        </div>
+    )
+}
+
+export default City;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import City from './app/index';
 
-ReactDOM.render(<h1>HELLO WORLD</h1>, document.getElementById('root'));
+ReactDOM.render(<City/>, document.getElementById('root'));
 
